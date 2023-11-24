8e6b6ce5a9ed0a5855e5e87292ec7fe27a801678
David jimenez <ederjimeneztapias@gmail.com>
2023-11-23 19:53:38 -0500

    actual23/11/2023

---
+++ tipodedatos.js
@@ -0,0 +1,26 @@
+//cadena de texto.
+const nombre = 'pepe';
+
+//number
+let num = 28;
+
+//boolean
+const mayor = true;
+const nomayor = false;
+
+//arrays
+const arreglos = ['Años', 22, true, {propiedad: 'valor'} [1, 2, 3]];
+
+//objetos
+const usuario = {
+    nombre: 'darlin',
+    apellido: 'fish',
+    edad: 22,
+    email: 'dar@gmail.comm',
+    auto: {
+        marca: 'BMW',
+        tipo: 'deportivo',
+        color: 'gris',
+    },
+};
+console.log(usuario);//si quiere acceder solo a un dato en especifico entonces es asi: console.log(usuario.nombre); o aun mas adentro es asi:console.log(usuario.auto.color);
