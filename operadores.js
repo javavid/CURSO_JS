0668868900b509dcd634bcc9c9751a6043935125
David jimenez <ederjimeneztapias@gmail.com>
2023-11-23 21:21:46 -0500

    actual23/11/2023

---
+++ operadores.js
@@ -0,0 +1,20 @@
+//operadores aritmeticos.
+/*
+= de asignacion.
++ suma.
+- resta.
+* multiplicacion.
+/ division.
+% modulo.
+++ incremento.
+-- decremento.
+*/
+
+//Operadores de asignacion.
+/*
++= suma un numero al valor de una variable.
+-= resta un numero al valor de una variable.
+*= multiplica un numero al valor de una variable.
+/= divide un numero al valor de una variable.
+%= obtiene el sobrante de una divicion y lo asigna a la variable.
+*/
