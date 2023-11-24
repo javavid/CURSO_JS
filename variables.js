3a662feb9fe0607b1b36a705b346732660bbd332
David jimenez <ederjimeneztapias@gmail.com>
2023-11-23 19:15:54 -0500

    actual23/11/2023

--- variables.js
+++ variables.js
@@ -3,3 +3,17 @@ let nombre = 'jerson';
 const email = 'jendrill@Gmail.com';
 
 console.log('Nombre: ' + nombre + '  Edad: ' + edad + '  Email: ' +  email);
+
+let pc, marca, color;
+pc = 'laptop';
+marca = 'asus';
+color = 'gris';
+console.log('PC: ' + pc + '  Marca: ' + marca + '  Color: ' + color); 
+
+let name = "david";
+let mensaje = `Hola ${name}`;
+console.log(mensaje);
+
+let suma = `suma ${3 + 5}`;
+console.log(suma);
+
