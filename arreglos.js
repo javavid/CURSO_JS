f4dc1d1a5dd0f4f553c3fde5110824818b1ceeff
David jimenez <ederjimeneztapias@gmail.com>
2023-11-23 23:09:06 -0500

    actual23/11/2023

---
+++ arreglos.js
@@ -0,0 +1,6 @@
+const arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(arreglo);
+arreglo.push(22);//push para agregar valor al final del array.
+console.log(arreglo);
+arreglo.pop();//Elimina el ultimo elemento del array.
+console.log(arreglo);
