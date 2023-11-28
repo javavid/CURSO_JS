df6064f9b31bd482cc4611fdafc71f84111e0ad8
David jimenez <ederjimeneztapias@gmail.com>
2023-11-28 18:13:36 -0500

    actual28/11/2023

--- operadorternario.js
+++ operadorternario.js
@@ -1,5 +1,5 @@
 const boleto = 'regular';
 let codigoDeAcceso;
 
-if(boleto === 'vip'? codigoDeAcceso = 'vip-123-456' : codigoDeAcceso = 'regular-789-101112');
+codigoDeAcceso = (boleto === 'vip') ? 'vip-123-456' : 'regular-789-101112';
 console.log(codigoDeAcceso);
