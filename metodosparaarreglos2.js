1c2ebd7a0f0a998c40fd9f8c865ed9be531d0d8c
David jimenez <ederjimeneztapias@gmail.com>
2023-11-29 18:44:29 -0500

    actual29/11/2023

---
+++ metodosparaarreglos2.js
@@ -0,0 +1,8 @@
+/* .indexOf  obtenemos el primer index de un elemento.
+ si no hay elemento no retorna -1.
+*/
+const nombres = ['brian','loki','thor','holck','batman','robin','spiderman','venon'];
+console.log(nombres.indexOf('holck'));
+
+// .lastIndexOf()  obtenemos el ultimo index de un elemento.
+console.log(nombres.indexOf('holck'));
