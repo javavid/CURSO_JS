44005284040a7e62922e26eafa93b6ddf0bc576e
David jimenez <ederjimeneztapias@gmail.com>
2023-11-28 22:07:32 -0500

    actual28/11/2023

--- metodosparaarreglos.js
+++ metodosparaarreglos.js
@@ -1,4 +1,4 @@
-const colores = ['rojo','verde','azul'];
+const colores = ['binotinto','cafe','azul'];
 // .length (propiedad) nos permite conocer la cantidad de elementos de un arreglo.
 console.log(colores.length);
 
@@ -9,3 +9,6 @@ document.body.innerHTML = colores.toString();//esto sale en la pantalla del nave
 
 // .join()  nos permite transformar un arreglo a una cadena de texto y separar cada elemento.
 console.log(colores.join('_'));
+
+// .sort()  nos permite ordenar un arreglo de cadenas de texto, de forma alfabetica.
+console.log(colores.sort());
