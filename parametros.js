452ce9d6ae5835be9bc662a5a8b8b24ab56fade8
David jimenez <ederjimeneztapias@gmail.com>
2023-11-28 19:05:00 -0500

    actual28/11/2023

---
+++ parametros.js
@@ -0,0 +1,12 @@
+const saludo = (nombre) => {
+    console.log(`hola ${nombre}`);
+}
+saludo('santi');
+saludo('vanesa');
+saludo('arllyn');
+
+//Multiples parametros.
+const operacion = (num1, num2) => {
+    console.log(num1 + num2);
+}
+operacion(5,5);
