ffa8f363e40f0688e3123a2956aaa4bf62f61ac8
David jimenez <ederjimeneztapias@gmail.com>
2023-11-29 18:55:43 -0500

    actual29/11/2023

--- metodosparaarreglos2.js
+++ metodosparaarreglos2.js
@@ -1,8 +1,13 @@
 /* .indexOf  obtenemos el primer index de un elemento.
  si no hay elemento no retorna -1.
 */
-const nombres = ['brian','loki','thor','holck','batman','robin','spiderman','venon'];
-console.log(nombres.indexOf('holck'));
+const nombres = ['brian','loki','thor','holck','batman','robin','spiderman','venon','thor'];
+console.log(nombres.indexOf('thor'));
 
 // .lastIndexOf()  obtenemos el ultimo index de un elemento.
-console.log(nombres.indexOf('holck'));
+console.log(nombres.lastIndexOf('thor'));
+
+// .forEach()  nos permite ejecutar una funcion por cada elemento.
+nombres.forEach((nombre) => {
+console.log(`hola ${nombre}`);
+});
