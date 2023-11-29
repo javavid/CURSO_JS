87bab4c210c0889175f068a15fe1d071b3b95acf
David jimenez <ederjimeneztapias@gmail.com>
2023-11-28 22:37:20 -0500

    actual28/11/2023

--- metodosparaarreglos.js
+++ metodosparaarreglos.js
@@ -15,3 +15,16 @@ console.log(colores.sort());//este .sort() tambien ordena numeros.
 
 // .reverse()  nos permite ordenar un arreglo de forma decendente.
 console.log(colores.reverse());//este .reverse() tambien ordena numeros de forma decendente.
+
+// .concat()  nos permite juntar dos arreglos en uno solo.
+const letras = ['a','b','c'];
+const numeros = [1,2,3];
+console.log(letras.concat(numeros));
+
+// .push()  nos permite agregar un elemento al final de un arreglo.
+colores.push('dorado');
+console.log(colores);
+
+// .pop()  nos permite eliminar el ultimo elemento de un arreglo.
+colores.pop();
+console.log(colores);
