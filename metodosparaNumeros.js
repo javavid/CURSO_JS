ffb09f6cc3324c3d15ab236adb435a5f8623734c
David jimenez <ederjimeneztapias@gmail.com>
2023-11-30 13:14:49 -0500

    actual30/11/2023

---
+++ metodosparaNumeros.js
@@ -0,0 +1,6 @@
+// .toString()  convierte un numero a cadena de texto.
+const numero = 10;
+console.log(numero, typeof numero);
+
+const texto = numero.toString();
+console.log(texto, typeof texto);
