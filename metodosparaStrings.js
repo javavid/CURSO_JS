24b8816c7111369ab0259b09c80b89ab0f14122c
David jimenez <ederjimeneztapias@gmail.com>
2023-11-29 22:38:51 -0500

    actual29/11/2023

---
+++ metodosparaStrings.js
@@ -0,0 +1,10 @@
+// propiedad length.
+// devuelve el numero de caracteres de una cadena de texto.
+const texto = 'Ornitorrinco';
+console.log('La cadena de texto tiene: ' + texto.length + ' caracteres.');
+
+//.indexOf() y .lastIndexOf() devuelve el index del primer/ultimo caracter especificado.
+const tex = 'Ornitorrinco';
+//solo aparese la posiscion del caracter.
+console.log(tex.indexOf('r'));
+console.log(tex.lastIndexOf('r'));
