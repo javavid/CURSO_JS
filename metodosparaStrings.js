ce529ab919e5953699092e977d5ee430560f72dc
David jimenez <ederjimeneztapias@gmail.com>
2023-11-30 13:03:10 -0500

    actual30/11/2023

--- metodosparaStrings.js
+++ metodosparaStrings.js
@@ -16,3 +16,18 @@ console.log(tex.lastIndexOf('r'));
 const textos = 'Ornitorrinco';
 const indice = textos.lastIndexOf('r');
 console.log(textos.slice(indice));//corto desde la posicion 7, si vas a poner el parametro en numero.
+
+/* .replace()  devuelve una cadena de texto donde remplaza un valor por otro.
+- 1er parametro - el texto que queremos remplazar.
+- 2do parametro - el texto que queremos poner.
+*/
+const nom = 'tiranosauriorex';
+console.log(nom.replace('rex', 'gay'));
+
+// .split()  convierte una cadena de texto en un arreglo.
+// tenemos que especificar donde cortar cada elemento.
+// 1er parametro - el cararcter que funcionara como separador.
+const txt = 'escolopendra';
+console.log(txt.split('e'));
+
+// .toUppercase()  .toLowercase()  devuelve una cadena de texto de puras Mayusculas/Minusculas.
