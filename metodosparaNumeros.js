4d78a1c4d7c728de93e0653e569454d2f66643d2
David jimenez <ederjimeneztapias@gmail.com>
2023-11-30 15:01:58 -0500

    actual30/11/2023

--- metodosparaNumeros.js
+++ metodosparaNumeros.js
@@ -10,8 +10,39 @@ const num = 3.14159265358979323846;
 console.log(num.toFixed(4));//se pone la cantidad de decimales que quiere mostrar despues del punto.
 
 
-//parseInt() es una funcion, intenta transformar un valor a un entero.
-// no se puede trabajar con decimales.
+/*parseInt() es una funcion, intenta transformar un valor a un entero.
+ no se puede trabajar con decimales.
 const nume1 = parseInt(prompt('escribe nume1'));
 const nume2 = parseInt(prompt('escribe nume2'));
 console.log(nume1 + nume2); 
+*/
+
+/* parseFloat() intenta transformar un valor a un numero con decimales.
+const nume1 = parseFloat(prompt('escribe nume1'));
+const nume2 = parseFloat(prompt('escribe nume2'));
+console.log(nume1 + nume2); 
+*/
+
+
+// Math.random()  genera un numero al azar enetre 0 y 1.
+const N = Math.random();
+console.log(N);
+
+// Math.floor()  redondea hacia abajo un numero.
+console.log(Math.floor(10.5));
+console.log(Math.floor(10.999));
+
+
+// Math.ceil()  redondea hacia arriba un numero.
+console.log(Math.ceil(10.2));
+console.log(Math.ceil(10.999));
+
+
+// Math.round()  redondea hacia el entero mas cercano.
+console.log(Math.round(10.2));
+console.log(Math.round(10.999));
+
+
+// Ejemplo de numeros al azar de l 0 al 100.
+const Nr = Math.random();
+console.log(Math.floor(Nr * 101));
