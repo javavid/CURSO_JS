92755c42deba5e756deab674ebc357e7e0662b57
David jimenez <ederjimeneztapias@gmail.com>
2023-11-29 22:47:41 -0500

    actual29/11/2023

--- metodosparaStrings.js
+++ metodosparaStrings.js
@@ -8,3 +8,10 @@ const tex = 'Ornitorrinco';
 //solo aparese la posiscion del caracter.
 console.log(tex.indexOf('r'));
 console.log(tex.lastIndexOf('r'));
+
+/* .Slice()  devuelve un fragmento de una cadena de texto.
+- 1er parametro - index desde dodne queremos cortar.
+- 2do parametro - (opcional) index hasta dodne queremos cortar.
+*/
+const textos = 'Ornitorrinco';
+console.log(textos.slice(7));//corto desde la posicion 7.
