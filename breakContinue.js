d670034a30830c8771817b69ce3cb460dfaec49a
David jimenez <ederjimeneztapias@gmail.com>
2023-11-30 18:11:11 -0500

    actual30/11/2023

--- breakContinue.js
+++ breakContinue.js
@@ -3,6 +3,7 @@ const nom = ['Armando','Alex','Arturo','Alexis','Amparo','Rosembell','Andres','A
 for(let i = 0; i < nom.length; i++){
 if (nom[i][0] !== 'A'){// nom[i][0] accedemos  a [i] es la posicion, y a [0] es la primera letra de cada nombre.
     console.log('Alto hay un nombre que no empieza por la letra A');
+    console.log(nom[i] + ' no empieza por la letra A');
 break;
 }
 console.log(nom[i]);
