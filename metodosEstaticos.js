b5b3026434c0b254c1fc2ea85108ce2b3c932db8
David jimenez <ederjimeneztapias@gmail.com>
2023-11-30 22:25:31 -0500

    actual30/11/2023

---
+++ metodosEstaticos.js
@@ -0,0 +1,14 @@
+class Usuario{
+    static num = 222;
+
+    constructor(nombre, email){
+        this.name = nombre;
+        this.correo = email;
+    }
+    
+    static borrar(id_user){ // metodo statico.
+        console.log(`El usuario con el id ${id_user} ha sido borrado de la base de datos`);
+    }
+}
+Usuario.borrar(3); // de esta manera se llama un metodo estatico de una clase.
+console.log(Usuario.num);
