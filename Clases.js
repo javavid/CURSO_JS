a4124071389f740a65c1a71215efc510cd760150
David jimenez <ederjimeneztapias@gmail.com>
2023-11-30 20:57:09 -0500

    actual30/11/2023

---
+++ Clases.js
@@ -0,0 +1,22 @@
+/* Estrutura de una clase:
+- Definicion
+- propiedades: la clase puede contener variables. dentro de una clase se llaman propiedades.
+- constructor: es un metodo especial para inicializar un objeto creado a partir de una clase.
+- metodos: la clase puede tener funciones. dentro de una clase se llaman metodos.
+*/
+class Usuario {
+ tipo = 'usuario';
+
+ constructor(nombre, apellido){
+this.nom = nombre;
+this.ape = apellido;
+ }
+ obtenerNombreCompleto(){
+    return `${this.nom} ${this.ape}`;
+ }
+}
+const user = new Usuario('david','hernandez');
+console.log(user);
+
+const user2 = new Usuario('julieta','mendez');
+console.log(user2.obtenerNombreCompleto());
