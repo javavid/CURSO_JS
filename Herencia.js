5dfc1e4c03e1623d1715b18bd73d2889de2b8940
David jimenez <ederjimeneztapias@gmail.com>
2023-11-30 22:03:10 -0500

    actual30/11/2023

--- Herencia.js
+++ Herencia.js
@@ -14,10 +14,19 @@ constructor(usuario, password, permisos){
 super(usuario, password); // con super() podemos copiar todos los metodos, propiedades y  constructores,
 this.permi = permisos;    // de la clase padre ose Usuario.
 }
+borrarpost(id){
+if(this.permi.includes('borrar')){
+console.log(`El post con el ${id} ha sido borrado`);
+}else{
+    console.log('No tienes los permisis para borrar post');
+}
+}
 }
 
 const users = new Ususario('pedro', 54321);
 console.log(users);
 
 const users2 = new Moderador('maria', 109876, ['borrar','editar']);//arreglo que contenga los permisos.
-console.log(users);
+console.log(users2.obtenerposts());
+console.log(users2.permi);
+users2.borrarpost(8);
