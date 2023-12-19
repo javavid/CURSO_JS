5f46d1c6e9ec10684a33d71084a166aae67fe3f3
David jimenez <ederjimeneztapias@gmail.com>
2023-12-19 17:47:29 -0500

    actual19/12/2023

---
+++ consola_nodejs.js
@@ -0,0 +1,16 @@
+let num1 = 6;
+let num2 = 5;
+let num3 = num1 + num2;
+console.log(num3);
+
+
+
+//en la terminal se puede ver los resultados de consola con nodejs.
+
+/*
+Esto es codigo en la terminal con gitbash.
+
+saram@LAPTOP-KAS1B2NJ MINGW64 /c/xampp/htdocs/CURSO_JS (main)
+$ node consola_nodejs.js
+11
+*/
