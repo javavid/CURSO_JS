daf8e14bd9d0da3b3fae0a82479e402b34c6e2bc
David jimenez <ederjimeneztapias@gmail.com>
2023-12-19 22:27:09 -0500

    actual19/12/2023

---
+++ Math.js
@@ -0,0 +1,8 @@
+Math.E           // devuelve el numero de euler
+Math.PI          // devuelve PI
+Math.SQRT2       // devuelve la raiz cuadrada de 2
+Math.SQRT1_2     // devuelve la rais cuadrada de 1/2
+Math.LN2         // devuelve el logaritmo natural de 2
+Math.LN10        // devuelve el logaritmo natural de 10
+Math.LOG2E       // devuelve el logaritmo de E en base 2
+Math.LOG10E      // devuelve el logaritmo de E en base 10
